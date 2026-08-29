8f95e237c74207f09fa8f66a0ef37abd90a27d23
Bagos Bimo Yekti Wibowo <138282093+ABCreativeOfficial@users.noreply.github.com>
2026-08-29 15:32:04 +0700

    feat(newapi): add referral reward transfer-all

--- src/dashboardGuard.js
+++ src/dashboardGuard.js
@@ -229,7 +229,7 @@ export { isAuthenticated };
 function isSensitiveDashboardPath(pathname) {
   if (SENSITIVE_DASHBOARD_PATHS.some((p) => pathname.startsWith(p))) return true;
   // This action mints a v2 secret that can lease one stored management PAT.
-  return /^\/api\/usage\/[^/]+\/newapi-checkin$/.test(pathname);
+  return /^\/api\/usage\/[^/]+\/newapi-(checkin|referral)$/.test(pathname);
 }
 
 export const __test__ = {
